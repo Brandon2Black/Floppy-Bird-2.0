0c1269d24aacd1b0e05cef2c4d672e8b18b7d449
Brandon2Black <brandonb627@nycstudents.net>
2025-05-09 09:21:01 -0400

    .

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 
     Rigidbody2D bird;
+    int score = 0;
+    public Text scoreUI;
     // Start is called before the first frame update
   void Start()
 {
@@ -15,7 +18,16 @@ public class PlayerController : MonoBehaviour
 
 }
 
+void OnTriggerEnter2D(Collider2D other)
+{
+    if(other.gameObject.CompareTag("Points"))
+    {
+        // Increase da score by one
+        score += 1;
 
+        scoreUI.text = score.ToString();
+    }
+}
 
 
     // Update is called once per frame
